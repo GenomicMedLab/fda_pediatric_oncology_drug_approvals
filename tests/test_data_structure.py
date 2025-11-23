abf24e89e309be607c219e9dda2b0ad1b3b8955e
James Stevenson <james.stevenson@nationwidechildrens.org>
2025-11-22 20:15:45 -0500

    test: add check for onset term phrasing

--- tests/test_data_structure.py
+++ tests/test_data_structure.py
@@ -1,11 +1,27 @@
 from pathlib import Path
+import re
 import json
+
+import pytest
 
 from ga4gh.va_spec.base import Statement
 
 
-def test_valid_data_structure():
+@pytest.fixture(scope="session")
+def data():
     with (Path(__file__).parents[1] / "fda_poda.json").open() as f:
-        data = json.load(f)
+        return json.load(f)
+
+
+def test_valid_data_structure(data: dict):
     for statement in data.get("statements", []):
         assert Statement(**statement)
+
+
+def test_age_phenotype_format(data: dict):
+    age_of_onset_pattern = re.compile(r"^\d+ (months|years) and older$")
+    for statement in data.get("statements", []):
+        for condition in statement["proposition"]["conditionQualifier"]["conditions"]:
+            if condition["id"].startswith("fda_poda.onset"):
+                assert condition["id"].split(":")[-1] == condition["name"]
+                assert re.match(age_of_onset_pattern, condition["name"])
