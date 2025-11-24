415dcc980e7744ec2d155d44bd7da0463b00f2a7
James Stevenson <james.stevenson@nationwidechildrens.org>
2025-11-24 09:22:45 -0500

    this is simpler

--- tests/test_data_structure.py
+++ tests/test_data_structure.py
@@ -20,6 +20,4 @@ def test_valid_data_structure(data: dict):
 def test_statement_ids_successive(data: dict):
     """Test that statement IDs are unique and incrementing"""
     id_values = [int(s["id"].split(":")[-1]) for s in data.get("statements", [])]
-    assert list(set(id_values)) == id_values
-    assert all(id_value > 0 for id_value in id_values)
-    assert len(id_values) == max(id_values)
+    assert id_values == list(range(1, max(id_values) + 1))
