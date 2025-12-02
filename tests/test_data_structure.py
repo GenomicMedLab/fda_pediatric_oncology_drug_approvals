1902e1a33f2bc4912d241c73ae16672d50f5963e
James Stevenson <james.stevenson@nationwidechildrens.org>
2025-12-02 09:27:30 -0500

    test: add check that statement IDs are incrementing and unique (#14)

--- tests/test_data_structure.py
+++ tests/test_data_structure.py
@@ -1,11 +1,23 @@
 from pathlib import Path
 import json
 
+import pytest
+
 from ga4gh.va_spec.base import Statement
 
 
-def test_valid_data_structure():
+@pytest.fixture(scope="session")
+def data():
     with (Path(__file__).parents[1] / "fda_poda.json").open() as f:
-        data = json.load(f)
+        return json.load(f)
+
+
+def test_valid_data_structure(data: dict):
     for statement in data.get("statements", []):
         assert Statement(**statement)
+
+
+def test_statement_ids_successive(data: dict):
+    """Test that statement IDs are unique and incrementing"""
+    id_values = [int(s["id"].split(":")[-1]) for s in data.get("statements", [])]
+    assert id_values == list(range(1, max(id_values) + 1))
