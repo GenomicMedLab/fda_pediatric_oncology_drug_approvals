7dc8169c70227a00eb80c7fd0b3bcf03d10ff2f0
James Stevenson <james.stevenson@nationwidechildrens.org>
2025-12-09 10:39:45 -0500

    test: require consistent phrasing of age of onset term (#11)

--- tests/test_data_structure.py
+++ tests/test_data_structure.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import re
 import json
 
 import pytest
@@ -15,6 +16,18 @@ def data():
 def test_valid_data_structure(data: dict):
     for statement in data.get("statements", []):
         assert Statement(**statement)
+
+
+def test_age_phenotype_format(data: dict):
+    age_of_onset_pattern = re.compile(r"^\d+ (?:months?|years?) and older$")
+    for statement in data.get("statements", []):
+        for condition in statement["proposition"]["conditionQualifier"]["conditions"]:
+            if condition["id"].startswith("fda_poda.onset"):
+                assert condition["id"].split(":")[-1] == condition["name"]
+                assert (
+                    re.match(age_of_onset_pattern, condition["name"])
+                    or condition["name"] == "Pediatric"
+                )
 
 
 def test_statement_ids_successive(data: dict):
